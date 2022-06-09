b75c7ff49f351b53d0fae6f10dd418528d5e6d83
testdev <83214438+DanteDEVS@users.noreply.github.com>
2022-06-09 18:06:39 +0200

    fix

--- src/mm/tasks/ArrowTask.php
+++ src/mm/tasks/ArrowTask.php
@@ -6,12 +6,14 @@ use mm\game\Game;
 use pocketmine\scheduler\Task;
 
 class ArrowTask extends Task{
+    
+    public $plugin;
 
     public function __construct(Game $plugin){
         $this->plugin = $plugin;
     }
 
-    public function onRun(int $ct){
+    public function onRun(){
         $this->plugin->giveArrow();
     }
 }
