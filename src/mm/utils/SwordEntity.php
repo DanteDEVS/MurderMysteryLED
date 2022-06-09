97dbbb20f7140107555a36f706b7ece505153a16
testdev <83214438+DanteDEVS@users.noreply.github.com>
2022-06-09 18:06:15 +0200

    PM4  Updates

--- src/mm/utils/SwordEntity.php
+++ src/mm/utils/SwordEntity.php
@@ -2,29 +2,34 @@
 
 namespace mm\utils;
 
-use pocketmine\entity\Entity;
 use pocketmine\player\Player;
 use pocketmine\network\mcpe\protocol\MobEquipmentPacket;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStackWrapper;
+use pocketmine\entity\EntitySizeInfo;
 use pocketmine\item\Item;
+use pocketmine\item\ItemIds;
+use pocketmine\entity\projectile\Throwable;
 
-class SwordEntity extends Entity{
-    public const NETWORK_ID = self::ARMOR_STAND;
-
-    public $width = 1.5;
-    public $height = 1.5;
+class SwordEntity extends Throwable{
+    public static function getNetworkTypeId() : string{ return EntityIds::ARMOR_STAND; }
     
+    public $propertyManager;
+    
+   protected function getInitialSizeInfo() : EntitySizeInfo{ return new EntitySizeInfo(1.5, 1.5); }	
+	
     protected function sendSpawnPacket(Player $player) : void{
         parent::sendSpawnPacket($player);
         $pk = new MobEquipmentPacket();
-        $pk->entityRuntimeId = $this->getId();
-        $pk->item = ItemStackWrapper::legacy(new Item(\pocketmine\item\ItemIds::IRON_SWORD));
+        $pk->actorRuntimeId = $this->getId();
+        $pk->item = ItemStackWrapper::legacy(new Item(ItemIds::IRON_SWORD));
         $pk->inventorySlot = 0;
         $pk->hotbarSlot = 0;
         $player->getNetworkSession()->sendDataPacket($pk);
     }
 
     public function setPose() : void{
-        $this->propertyManager->setInt(self::DATA_ARMOR_STAND_POSE_INDEX, 8);
+        $this->propertyManager->setInt(EntityMetadataProperties::ARMOR_STAND_POSE_INDEX, 8);
     }
 }
