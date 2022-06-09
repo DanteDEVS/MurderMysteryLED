7872ce54544307b0b87d17bb964fda99dff294ec
testdev <83214438+DanteDEVS@users.noreply.github.com>
2022-06-09 18:04:19 +0200

    Fix

--- src/mm/MurderMystery.php
+++ src/mm/MurderMystery.php
@@ -44,7 +44,9 @@ class MurderMystery extends PluginBase implements Listener{
         $this->provider->loadGames();
 	    EntityFactory::getInstance()->register(SwordEntity::class, function(World $world, CompoundTag $nbt) : SwordEntity{
 		          return new SwordEntity(EntityDataHelper::parseLocation($nbt, $world), null, $nbt);
-	    }, ['SwordEntity']);           
+	    }, ['SwordEntity']);       
+	    
+    public function onCommand(CommandSender $sender, Command $cmd, string $str, array $args) : bool{
         if(strtolower($cmd) == "murdermystery"){
             if(count($args) == 0){
                 $sender->sendMessage("§7Use §b/murdermystery help");
