beee22cda0adca7fbb24316c0341d3fa85c8f31f
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-07-23 23:30:51 +0200

    Better changes to SwordEntity

--- src/mm/utils/SwordEntity.php
+++ src/mm/utils/SwordEntity.php
@@ -2,35 +2,44 @@
 
 namespace mm\utils;
 
+use pocketmine\entity\Entity;
+use pocketmine\entity\EntitySizeInfo;
+use pocketmine\item\ItemIds;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
+use pocketmine\network\mcpe\protocol\types\inventory\ItemStack;
 use pocketmine\player\Player;
 use pocketmine\network\mcpe\protocol\MobEquipmentPacket;
-use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
-use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStackWrapper;
-use pocketmine\entity\EntitySizeInfo;
 use pocketmine\item\Item;
-use pocketmine\item\ItemIds;
-use pocketmine\entity\projectile\Throwable;
 
-class SwordEntity extends Throwable{
-	
-   public $networkProperties;
+class SwordEntity extends Entity{
 
-   public static function getNetworkTypeId() : string{ return EntityIds::ARMOR_STAND; }
+    public static function getNetworkTypeId(): string
+    {
+        return self::NETWORK_ID;
+    }
+
+    protected function getInitialSizeInfo(): EntitySizeInfo
+    {
+        return new EntitySizeInfo($this->height, $this->width);
+    }
+
+    public const NETWORK_ID = "minecraft:armor_stand";
+
+    public $width = 2.0;
+    public $height = 2.0;
     
-   protected function getInitialSizeInfo() : EntitySizeInfo{ return new EntitySizeInfo(1.5, 1.5); }	
-	
     protected function sendSpawnPacket(Player $player) : void{
         parent::sendSpawnPacket($player);
         $pk = new MobEquipmentPacket();
         $pk->actorRuntimeId = $this->getId();
-        $pk->item = ItemStackWrapper::legacy(new Item(ItemIds::IRON_SWORD));
+        $pk->item = ItemStackWrapper::legacy(new ItemStack(ItemIds::IRON_SWORD, 0, 1, 0,null, null, null));
         $pk->inventorySlot = 0;
         $pk->hotbarSlot = 0;
-        $player->getNetworkSession()->sendDataPacket($pk);
+        $player->sendData($this->getViewers(), [$pk]);
     }
 
     public function setPose() : void{
-        $this->networkProperties->setInt(EntityMetadataProperties::ARMOR_STAND_POSE_INDEX, 8);
+        $this->getNetworkProperties()->setInt(EntityMetadataProperties::ARMOR_STAND_POSE_INDEX, 8);
     }
 }
